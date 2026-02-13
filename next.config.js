9d7ffd7b6828d844b5d9db42056bdf043908833a
GamingNJncos <106001964+GamingNJncos@users.noreply.github.com>
2026-02-13 02:36:32 -0600

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,22 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+/** @type {import('next').NextConfig} */
+module.exports = withBundleAnalyzer({
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/chunks/[path][name].[hash][ext]",
+      },
+    });
+
+    return config;
+  },
+  reactStrictMode: false,
+});
